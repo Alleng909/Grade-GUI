dd988008de9c3f1980b1915ff1e193c0ca2b1d1b
AllenG <alleng909@gmail.com>
2017-12-11 06:29:16 -0800

    Added recalculate button, but need to write code to grab the grade info from each slider I renamed for convenience and crunch it.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,8 +14,15 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_pushButton_clicked()
+void MainWindow::on_pushButton_clicked()//Info box for grading schemes
 {
     QMessageBox::information(this, "Grading Schemes",
         "A: 25% HW + 20% Midterm 1 + 20% Midterm 2 + 35% Final Exam \nB: 25% HW + 30% Highest Midterm + 44% Final Exam");
+}
+
+void MainWindow::on_pushButton_2_clicked() // Button to recalculate grades.
+{
+    int test = 99;
+    ui->redo1->setText(QString::number(test));
+    ui->redo2->setValue(test);
 }
